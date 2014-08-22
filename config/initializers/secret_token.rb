0f9df6f2581ff0c47956f99de1336b83566c645b
huicheese <huicheese@gmail.com>
2014-08-22 08:40:20 +0000

    added assets and html but does not load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OccupancyWeb::Application.config.secret_key_base = 'e3944f6476c792d0000d8fd5012075f1c8ad337d35b7b380214a6c1cae2aa4cdd1111f0f075c4471020c387b5ed555e5eec6abb475def5624c4dc3696fdf04ea'
