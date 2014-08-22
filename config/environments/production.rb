ca2a0f7063c02ebab80d7e784cf7f0cda24af747
huicheese <huicheese@gmail.com>
2014-08-22 06:14:21 +0000

    modified routes, applicationm production, development and test.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Occupancy_web_ver::Application.configure do
+OccupancyWeb::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-Occupancy_web_ver::Application.configure do
+OccupancyWeb::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
